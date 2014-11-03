bc6f110c179dd603cc4011c8ee5308906c417f7f
Gabe Roeloffs <gdroel@gmail.com>
2014-11-02 19:37:54 -0800

    tournament index styled

--- app/models/Tournament.php
+++ app/models/Tournament.php
@@ -2,5 +2,9 @@
 
 class Tournament extends Eloquent{
 
-	
+	public function getDates(){
+
+		    return array('created_at', 'updated_at', 'register_start', 'register_end', 'tournament_start', 'tournament_end');
+		}
+
 }
--- app/views/tournaments/tournamentsIndex.blade.php
+++ app/views/tournaments/tournamentsIndex.blade.php
@@ -13,16 +13,39 @@
 		<div class="row tournament">
 			<h2>{{ $tournament->name }}</h2>
 			<hr>
-			<h3>{{ $tournament->location}}</h3>
+			<h3>Location: {{ $tournament->location}}</h3>
+			<p><a href="{{ $tournament->website }}">Tournament Website</a></p>
 			<p>{{ $tournament->description }}</p>
-			<a href="{{ $tournament->website }}">Website</a>
-			<p>Register Start: {{ $tournament->register_start }}</p>
-			<p>Register End: {{ $tournament->register_end }}</p>
-			<p>Tournament Start: {{ $tournament->tournament_start }}</p>
-			<p>Tournament End: {{ $tournament->tournament_end }}</p>
+			<table class="table">
+				<thead>
+					<tr>
+						<th>Dates</th>
+						<th>Start</th>
+						<th>End</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>Registration</td>
+						<td>{{ $tournament->register_start->format('g:ia M j, Y') }}</td>
+						<td>{{ $tournament->register_end->format('g:ia M j, Y') }}</td>
+					</tr>
+					<tr>
+						<td>Tournament</td>
+						<td>{{ $tournament->tournament_start->format('M j, Y') }}</td>
+						<td>{{ $tournament->tournament_end->format('M j, Y') }}</td>
+					</tr>
+				</tbody>
+			</table>
+
 			</div>
 		@endforeach
 		
+	</div>
+	<div class="col-md-3">
+		@if(Auth::check())
+			<a href="{{ URL::to('tournaments/new') }}" class="btn btn-default sidebar-button">Add A Tournament</a>
+		@endif
 	</div>
 </div>
 @stop
